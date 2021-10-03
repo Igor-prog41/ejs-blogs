304afc0d0a2d1a86174e0386cf0f963c9081b9a6
igor iaroshevych <iaroshevych.i@gmail.com>
2021-10-03 15:10:15 -0600

    add about.ejs compose.ejs  contact.ejs

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const homeStartingContent = "Lorem ipsum dolor sit amet consectetur, adipisicing
 const aboutContent = "About Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem nesciunt odit obcaecati temporibus quisquam quod enim sapiente libero dolor quam repudiandae, sequi, excepturi consequatur rerum laudantium quo, nihil in architecto."
 const contactContent = "Contact Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem nesciunt odit obcaecati temporibus quisquam quod enim sapiente libero dolor quam repudiandae, sequi, excepturi consequatur rerum laudantium quo, nihil in architecto."
 
+const posts =[];
+
 const app =express();
 
 app.set("view engine", "ejs");
@@ -14,11 +16,38 @@ app.use(express.static("public"));
 
 
 app.get("/",(req,res)=>{
-    res.render("home.ejs");
+    res.render("home",{
+        paragraphHomePage:homeStartingContent,
+        arrPosts:posts
+    });
 })
 
-// git ignor add.
+app.get("/about",(req,res)=>{
+    res.render("about",{paragraphAboutPage:aboutContent});
+})
+
+app.get("/contact",(req,res)=>{
+    res.render("contact",{paragraphContactPage:contactContent});
+})
+
+app.get("/compose",(req,res)=>{
+    res.render("compose");
+})
+
+app.post("/compose",(req,res)=>{
+   
+    const newPost={
+        "title": req.body.postTitle,    
+        "body" : req.body.postBody  
+    }
+
+    posts.push(newPost);
+
+    // console.log(posts);
+    res.redirect("/");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 })
+
